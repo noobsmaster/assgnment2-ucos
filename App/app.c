a24fe0da4ffc358cd031645152a401cf8620e041
poteryu <ccmmelati@gmail.com>
2015-07-29 00:07:19 +0800

    yooo

--- App/app.c
+++ App/app.c
@@ -5,8 +5,8 @@
  
 #include <includes.h>
 
-INT32U i = 0;
-INT32U j = 0;
+INT16U led_stat=0;
+INT16U SW1_logic=1;
     
 #define TASK_1_PRIO       8
 #define TASK_2_PRIO       5
@@ -38,22 +38,100 @@ CPU_INT16S  main (void)
 	return (-1);             
 }
 
-static void Task_1(void *p_arg)
+static void Task_1(void *p_arg) //task 1 = de-bouncing
 {
   (void)p_arg;  /* avoid compiler warning */
   
-  while (1) {
-    i++;
-    i++;
+  while (1) 
+  {
+  	if(SW1_d!=SW1)
+	{
+		if(SW1_d==0)
+			SW1=0;
+		if(SW1_d==1)
+			SW1=1;
+		OSTimeDlyHMSM(0,0,0,20);
+	}
+
+	if(SW2_d!=SW2)
+	{
+		if(SW2_d==0)
+			SW2=0;
+		if(SW2_d==1)
+			SW2=1;
+		OSTimeDlyHMSM(0,0,0,20);
+	}
   }
 }
 
-static void Task_2(void *p_arg)
+static void Task_2(void *p_arg)	//led 3 blinking at 1 hz
 {
   (void)p_arg;  /* avoid compiler warning */
   
-  while (1) {
-    j++;
-    j++;  
+  while (1) 
+  {
+    LED3=~LED3;
+	OSTimeDlyHMSM(0,0,0,500);
   }
+}
+
+static void Task_3(void *p_arg)	//led 1 blinking state control
+{
+	(void)p_arg;  /* avoid compiler warning */
+	
+	while (1)
+	{
+		if(SW1==1)
+		{
+			SW1_logic=1;
+		}
+		if(SW1==0)
+		{
+			if(SW1_logic==1)
+			{
+				if(led_stat<3)
+				{
+					led_stat=led_stat+1;
+				}
+				if(led_stat==3)
+				{
+					led_stat=0;
+				}
+				OSTimeDlyResume(TASK_4_PRIO);
+			
+			}
+			SW1_logic=0;
+		}
+		OSTimeDlyHMSM(0,0,0,10);
+	}
+	
+}
+
+static void Task_4(void *p_arg)	//led 1 blinking timing
+{
+	(void)p_arg;  /* avoid compiler warning */
+	
+	while (1)
+	{
+		switch(led_stat)
+		{
+			case(0):
+				LED1=0;
+				OSTaskSuspend();
+				break;
+			case(1):
+				LED1=~LED1;
+				OSTimeDlyHMSM(0,0,0,500);
+				break;
+			case(2):
+				LED1=~LED1;
+				OSTimeDlyHMSM(0,0,0,250);
+				break;
+			case(3):
+				LED1=~LED1;
+				OSTimeDlyHMSM(0,0,0,125);
+				break;
+				
+		}
+	}
 }
